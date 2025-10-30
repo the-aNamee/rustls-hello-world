3a9b7be673334eecbc9957abb3104779e9c9807a
Anamee <anameeguy@gmail.com>
2025-10-30 15:08:46 -0800

    Basic server

---
+++ src/bin/server.rs
@@ -0,0 +1,94 @@
+use std::{
+    io::{self, Error as IoError},
+    net::ToSocketAddrs,
+    path::PathBuf,
+    sync::Arc,
+};
+
+use anyhow::Result;
+use argh::FromArgs;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer, pem::PemObject};
+use tokio::{
+    io::{AsyncWriteExt, copy, sink, split},
+    net::TcpListener,
+};
+use tokio_rustls::TlsAcceptor;
+
+/// Tokio Rustls server example
+#[derive(FromArgs)]
+struct Options {
+    /// bind addr
+    #[argh(positional)]
+    addr: String,
+
+    /// cert file
+    #[argh(option, short = 'c')]
+    cert: PathBuf,
+
+    /// key file
+    #[argh(option, short = 'k')]
+    key: PathBuf,
+
+    /// echo mode
+    #[argh(switch, short = 'e')]
+    echo_mode: bool,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let options: Options = argh::from_env();
+
+    let addr = options
+        .addr
+        .to_socket_addrs()?
+        .next()
+        .ok_or_else(|| IoError::from(io::ErrorKind::AddrNotAvailable))?;
+    let certs = CertificateDer::pem_file_iter(&options.cert)?.collect::<Result<Vec<_>, _>>()?;
+    let key = PrivateKeyDer::from_pem_file(&options.key)?;
+    let flag_echo = options.echo_mode;
+
+    let config = rustls::ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)?;
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+
+    let listener = TcpListener::bind(&addr).await?;
+
+    loop {
+        let (stream, peer_addr) = listener.accept().await?;
+        let acceptor = acceptor.clone();
+
+        let fut = async move {
+            let mut stream = acceptor.accept(stream).await?;
+
+            if flag_echo {
+                let (mut reader, mut writer) = split(stream);
+                let n = copy(&mut reader, &mut writer).await?;
+                writer.flush().await?;
+                println!("Echo: {} - {}", peer_addr, n);
+            } else {
+                let mut output = sink();
+                stream
+                    .write_all(
+                        &b"HTTP/1.0 200 ok\r\n\
+                                    Connection: close\r\n\
+                                    Content-length: 12\r\n\
+                                    \r\n\
+                                    Hello world!"[..],
+                    )
+                    .await?;
+                stream.shutdown().await?;
+                copy(&mut stream, &mut output).await?;
+                println!("Hello: {}", peer_addr);
+            }
+
+            Ok(()) as io::Result<()>
+        };
+
+        tokio::spawn(async move {
+            if let Err(err) = fut.await {
+                eprintln!("{:?}", err);
+            }
+        });
+    }
+}
